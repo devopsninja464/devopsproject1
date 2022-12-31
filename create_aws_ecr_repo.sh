5dbeda46dfcd3a5ac9b3e6ae8c0ec74f0711800e
devopsninja464 <devopsninja464@gmail.com>
2022-12-30 19:12:07 -0500

    update create aws ecr file

--- create_aws_ecr_repo.sh
+++ create_aws_ecr_repo.sh
@@ -15,8 +15,8 @@ TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query au
 # Create or replace registry secret
 #
 
-kubectl delete secret --ignore-not-found $SECRET_NAME
-kubectl create secret docker-registry $SECRET_NAME \
+sudo k3s kubectl delete secret --ignore-not-found $SECRET_NAME
+sudo k3s kubectl create secret docker-registry $SECRET_NAME \
  --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
  --docker-username=AWS \
  --docker-password="${TOKEN}" \
